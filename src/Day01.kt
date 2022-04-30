baa39dee96699248aa17115046c90c6652ce3663
Cristian Heitt <agustin_cristian.heitt@hotmail.com>
2022-04-30 16:22:51 -0300

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { v -> v.toInt() }
+            .windowed(2)
+            .count { (a, b) -> b > a }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { v -> v.toInt() }
+            .windowed(4)
+            .count { (a, _, _, d) -> d > a }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 }
